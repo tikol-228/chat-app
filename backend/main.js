6ae7a649f2ddfc96cc089fee7150139184d129fb
tikol-228 <foghop600@gmail.com>
2026-01-07 19:19:03 +0300

    Add PORT environment variable

--- backend/main.js
+++ backend/main.js
@@ -8,6 +8,8 @@ import { Server } from 'socket.io';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 const server = createServer(app);
 
